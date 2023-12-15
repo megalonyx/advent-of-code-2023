c27fd465791cbdb8c24c7b442af5d24ac41662fc
megalonyx <d@nieltiggemann.de>
2023-12-15 21:36:20 +0100

    day 13 part 2

---
+++ day_13/day_13_2.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def read_patterns(filename):
+    patterns = []
+    rows = []
+    with open(filename, 'r') as f:
+        while line := f.readline():
+            line = line.strip()
+            if line == '':
+                patterns.append(rows)
+                rows = []
+            else:
+                cols = [ 1 if c == '#' else 0 for c in line ]
+                rows.append(cols)
+    patterns.append(rows)
+    return patterns
+
+def array_from_pattern(pattern):
+    arr = np.array(pattern)
+    return arr
+
+# hrow = 1 .. maxrow
+def is_horizontal_smudged(arr, hrow):
+    maxrow, maxcol = arr.shape
+    numtoprows = hrow
+    numbottomrows = maxrow - hrow
+    numrows = min(numtoprows, numbottomrows)
+    numdiffs = 0
+    for r in range(0, numrows):
+        for c in range(0, maxcol):
+            numdiffs += 0 if arr[hrow-r-1][c] == arr[hrow+r+1-1][c] else 1
+    return numdiffs == 1
+
+# vcol = 1 .. maxcol
+def is_vertical_smudged(arr, vcol):
+    maxrow, maxcol = arr.shape
+    numleftcols = vcol
+    numrightcols = maxcol - vcol 
+    numcols = min(numleftcols, numrightcols)
+    numdiffs = 0
+    for c in range(0, numcols):
+        for r in range(0, maxrow):
+            numdiffs += 0 if arr[r][vcol-c-1] == arr[r][vcol+c+1-1] else 1
+    return numdiffs == 1
+
+def score_for_array(arr):
+    maxrow, maxcol = arr.shape
+    s = 0
+    for col in range(1, maxcol):
+        if is_vertical_smudged(arr, col):
+            s += col
+    for row in range(1, maxrow):
+        if is_horizontal_smudged(arr, row):
+            s += 100 * row
+    return s
+
+def print_grid(grid):
+    rows, cols = grid.shape
+    for r in range(rows):
+        for c in range(cols):
+            v = grid[r, c]
+            p = '.' if v == 0 else '#'
+            print(p, end='')
+        print()
+
+def main():
+#    patterns = read_patterns('test.txt')
+    patterns = read_patterns('input.txt')
+    total = 0
+    for p in patterns:
+        arr = array_from_pattern(p)
+        score = score_for_array(arr)
+        total += score
+    print(total)
+
+if __name__ == '__main__':
+    main()
