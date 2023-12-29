fb55f2a14617c05e8388aba3ac13fd78f562e66f
megalonyx <d@nieltiggemann.de>
2023-12-29 21:39:32 +0100

    day 21 part 1 solution with queue and visited even/odd

--- day_21/day_21_1.py
+++ day_21/day_21_1.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-#import numpy as np
-
-STEPS_TO_REACH = 64
+from collections import deque
 
 def read_map(filename):
     rows = []
@@ -18,35 +16,40 @@ def starting_pos(rows):
                 return (r, c)
     return None
 
-def do_step(rows, pos, stepnumber, accum):
-    if stepnumber == STEPS_TO_REACH:
-        accum.add( pos )
-        return
-    r, c = pos
-    # north:
-    if r > 0 and rows[r-1][c] != '#':
-        do_step(rows, (r-1, c), stepnumber+1, accum)
-    # south:
-    if r < len(rows)-1 and rows[r+1][c] != '#':
-        do_step(rows, (r+1, c), stepnumber+1, accum)
-    # west:
-    if c > 0 and rows[r][c-1] != '#':
-        do_step(rows, (r, c-1), stepnumber+1, accum)
-    # east:
-    if c < len(rows[0])-1 and rows[r][c+1] != '#':
-        do_step(rows, (r, c+1), stepnumber+1, accum)
+def part1(rows, start, steps_left):
+    height = len(rows)
+    width = len(rows[0])
+    queue = deque([(start, steps_left)])
+    visited = set()
+    endpoints = set()
+    while queue:
+        current, steps_left = queue.popleft()
+        if steps_left >= 0:   # 0: reached end of this path
+            if steps_left % 2 == 0:  # even: we can always reach this; odd: impossible
+                endpoints.add(current)
+            if steps_left > 0: # advance to next position
+                steps_left -= 1
+                r, c = current
+                neighbours = []
+                if r > 0: neighbours.append( (r-1, c) )
+                if r < height-1: neighbours.append( (r+1, c) )
+                if c > 0: neighbours.append( (r, c-1) )
+                if c < width-1: neighbours.append( (r, c+1) )
+                for neigh in neighbours:
+                    r, c = neigh
+                    if neigh in visited or rows[r][c] == '#':
+                        continue
+                    else:
+                        queue.append( (neigh, steps_left) )
+                        visited.add( neigh )
+    return endpoints
 
 def main():
 #    rows = read_map('test.txt')
     rows = read_map('input.txt')
     start = starting_pos(rows)
-#    print(start)
-    accum = set()
-    do_step(rows, start, 0, accum)
-    print(len(accum))
-
-
-
+    res = part1(rows, start, 64)
+    print(len(res))
 
 if __name__ == '__main__':
     main()
